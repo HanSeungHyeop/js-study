23aa8f254661caee47187bab5a470451c541f2bf
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-12 18:53:29 +0900

    function in js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const a = 3;
-
-if (a === 5) {
-    console.log('5 입니다.');
-} else if (a === 10) {
-    console.log('10입니다.');
-} else {
-    console.log('왤캐 잠오지');
+function hello(name) {
+    console.log('Hello, ' + name + '!');
 }
+
+hello('bong8');
