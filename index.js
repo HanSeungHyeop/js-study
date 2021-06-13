22c78c814e75e1d5204218c853391c23e3c40d41
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-13 16:39:26 +0900

    getter & setter function

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const dog = {
-    _name: '멍멍이',
-    set name(value) {
-        console.log('이름이 바뀝니다..' + value);
-        this._name = value;
+const numbers = {
+    _a: 1,
+    _b: 2,
+    sum: 3,
+    calculate() {
+        console.log('calculate');
+        this.sum = this._a + this._b;
+    },
+    get a() {
+        return this._a;
+    },
+    get b() {
+        return this._b;
+    },
+    set a(value) {
+        this._a = value
+        this.calculate();
+    },
+    set b(value) {
+        this._b = value;
+        this.calculate();
     }
 }
-console.log(dog._name);
-dog.name = '몽몽이';
-console.log(dog._name);
+
+console.log(numbers.sum);
+numbers.a = 5;
+console.log(numbers.b);
