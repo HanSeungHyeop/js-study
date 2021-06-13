2e22dd117876732be6ffdb4d536553a04a95589a
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-13 16:31:26 +0900

    setter function

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const ironMan = {
-    name: '토니 스타크',
-    actor: '로버트 다우니 주니어',
-    alias: '아이언맨'
-};
-
-const captainAmerica = {
-    name: '스티븐 로저스',
-    actor: '크리스 에반스',
-    alias: '캡틴 아메리카'
-};
-
-function print({ alias, name, actor }) {
-    const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
-    console.log(text);
+const dog = {
+    _name: '멍멍이',
+    set name(value) {
+        console.log('이름이 바뀝니다..' + value);
+        this._name = value;
+    }
 }
-
-print(ironMan);
-print(captainAmerica);
+console.log(dog._name);
+dog.name = '몽몽이';
+console.log(dog._name);
