9eb1398e9ae927b7a1bf2ac63f86d0a15954e62d
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-13 15:52:49 +0900

    use => function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function hello(name) {
-    console.log('Hello, ' + name + '!');
+const add = (a, b) => {
+    return a + b;
+}
+const hello = (name) => {
+    return (`${name}씨 안녕하세요!!`);
 }
 
-hello('bong8');
+const sum = add(1, 3);
+console.log(sum);
+console.log(hello('승협'));
