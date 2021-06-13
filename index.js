ecf3c6498514607b125c101132845ab4376d4afa
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-13 16:02:28 +0900

    object

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const add = (a, b) => {
-    return a + b;
-}
-const hello = (name) => {
-    return (`${name}씨 안녕하세요!!`);
+const ironMan = {
+    name: '토니 스타크',
+    actor: '로버트 다우니 주니어',
+    alias: '아이언맨'
+};
+
+const captainAmerica = {
+    name: '스티븐 로저스',
+    actor: '크리스 에반스',
+    alias: '캡틴 아메리카'
+};
+
+function print(hero) {
+    const text = `${hero.alias}(${hero.name}) 역할을 맡은 배우는 ${hero.actor} 입니다.`;
+    console.log(text);
 }
 
-const sum = add(1, 3);
-console.log(sum);
-console.log(hello('승협'));
+print(ironMan);
+print(captainAmerica);
