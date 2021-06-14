8bf81fb08fd37fe7efbd5025c897510b1d34a68e
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-14 19:21:00 +0900

    for in js

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-const numbers = {
-    _a: 1,
-    _b: 2,
-    sum: 3,
-    calculate() {
-        console.log('calculate');
-        this.sum = this._a + this._b;
-    },
-    get a() {
-        return this._a;
-    },
-    get b() {
-        return this._b;
-    },
-    set a(value) {
-        this._a = value
-        this.calculate();
-    },
-    set b(value) {
-        this._b = value;
-        this.calculate();
-    }
-}
+const names = ['멍멍이', '야옹이', '멍뭉이'];
 
-console.log(numbers.sum);
-numbers.a = 5;
-console.log(numbers.b);
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
