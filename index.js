27c8b9debf1791c502ab2094acf3fe9c5c90b193
https://github.com/HanSeungHyeop/Study-JS.git <h970409@gmail.com>
2021-06-14 19:34:58 +0900

    for_of & for_in in js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const names = ['멍멍이', '야옹이', '멍뭉이'];
+const numbers = [10, 20, 30, 40, 50];
+let number;
+for (number of numbers) {
+    console.log(number);
+}
 
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
+const doggy = {
+    name: '멍멍이',
+    sound: '멍멍',
+    age: 2
+}
+
+console.log(Object.entries(doggy));
+console.log(Object.keys(doggy));
+console.log(Object.values(doggy));
+
+for (let key in doggy) {
+    console.log(key);
+}
+
+for (let key in doggy) {
+    console.log(`${key}: ${doggy[key]}`);
 }
